48b3a825df4ba93e2300d793a6e21495f2be946f
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-03-05 09:48:35 +0100

    Update chi

--- test-chi/main.go
+++ test-chi/main.go
@@ -9,26 +9,26 @@ import (
 )
 
 func main() {
+	// Channel to queue tasks
+	taskQueue := make(chan http.ResponseWriter, 100)
+
+	// Worker pool
+	for i := 0; i < 100; i++ {
+		go worker(taskQueue)
+	}
+
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
 
-	// Channel to queue tasks
-	taskQueue := make(chan http.ResponseWriter, 100)
-
-	// Worker pool
-	for i := 0; i < 10; i++ {
-		go worker(taskQueue)
-	}
-
-	r.Get("/spawn", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/worker", func(w http.ResponseWriter, r *http.Request) {
 		taskQueue <- w
 	})
 
-	r.Get("/spawn2", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/spawn", func(w http.ResponseWriter, r *http.Request) {
 		go func() {
-			time.Sleep(100 * time.Millisecond)
+			time.Sleep(500 * time.Millisecond)
 			w.Write([]byte("Hello, World!"))
 		}()
 	})
@@ -39,7 +39,7 @@ func main() {
 
 func worker(taskQueue chan http.ResponseWriter) {
 	for w := range taskQueue {
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(500 * time.Millisecond)
 		w.Write([]byte("Hello, World!"))
 	}
 }
